b9f365564b378c1aef2197a9e95296ae6e31b22f
MuhammadNurFaizin <muhammadnurfaizin611@gmial.com>
2021-02-27 23:22:12 +0700

    laporan

---
+++ praktikum3/SpaceGirl2/Assets/Script/Player.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+public class Player : MonoBehaviour
+{
+    public Text starText;
+    private bool carryingStar = false;
+    void Start()
+    {
+        UpdateStarText();
+    }
+    void OnTriggerEnter2D(Collider2D hit)
+    {
+        if (hit.CompareTag("star"))
+        {
+            carryingStar = true;
+            UpdateStarText();
+            Destroy(hit.gameObject);
+        }
+    }
+    private void UpdateStarText()
+    {
+        string starMessage = "Tidak Bawa Bintang :-(";
+        if (carryingStar) starMessage = "Bawa Bintang :-)";
+        starText.text = starMessage;
+    }
+}
---
+++ praktikum3/SpaceGirl2/Assets/Script/PlayerMove.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    public float speed = 10;
+    private Rigidbody2D rigidBody2D;
+    void Awake()
+    {
+        rigidBody2D = GetComponent<Rigidbody2D>();
+    }
+    void FixedUpdate()
+    {
+        float xMove = Input.GetAxis("Horizontal");
+        float yMove = Input.GetAxis("Vertical");
+        float xSpeed = xMove * speed;
+        float ySpeed = yMove * speed;
+        Vector2 newVelocity = new Vector2(xSpeed, ySpeed);
+        rigidBody2D.velocity = newVelocity;
+    }
+}
